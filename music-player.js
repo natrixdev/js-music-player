2e31d498b8840aad361d16ec056e358f2077ca55
natrix_dev <discordlenko@gmail.com>
2022-01-08 10:20:41 +0100

    Create music-player.js

---
+++ music-player.js
@@ -0,0 +1,30 @@
+//import a few modules 
+
+const math, html5, simpleVueJs, generator, exporter & modules = require("npm.io/natrix/list/vip/music-player")
+
+// let = new variable for your musics, insert a youtube url 
+let music1, music.$math.floor(1+1forEach) = "FIRST MUSIC - yt url only", "Another MUSIC", "Another MUSIC", "Another MUSIC", "Another MUSIC", "Another MUSIC", "Another MUSIC"
+
+// auto changer for musics 
+music.play & loop(queue)
+
+//generate a simple preview 
+
+html5.generator(preview.html) => assets: title;
+                                          desc;
+                                            footer;
+                                                music.looper();
+
+// for youtube reader and youtube music 
+
+!youtubeReade.decode(music.math(random)song =>&setButton&=>"Click Here To enbale auto-random-loop";color="BLUE"
+                     
+//fucntion random song
+                     
+ module.export(modules) 
+//just for my github stats ! 
+//No datas collected
+
+!natrix
+
+//by natrix dev may work with last current node version 
